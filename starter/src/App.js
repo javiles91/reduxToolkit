00c9d3f5eb1029553c66a049c3ddda815086acf1
Jose Aviles <aviles.yanes7@gmail.com>
2022-10-07 16:02:02 -0600

    cart feature completed with redux toolkit

---
+++ starter/src/App.js
@@ -0,0 +1,38 @@
+import NavBar from "./components/NavBar";
+import CardConntainer from "./components/CartContainer";
+import React from "react";
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { calculateTotals, getCartItems } from "./features/cart/cartSlice";
+import Modal from "./components/Modal";
+
+function App() {
+  const { cartItems, isLoading } = useSelector((state) => state.cart);
+  const { isOpen } = useSelector((state) => state.modal);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(calculateTotals());
+  }, [cartItems]);
+
+  useEffect(() => {
+    dispatch(getCartItems());
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div className="loading">
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
+
+  return (
+    <main>
+      {isOpen && <Modal />}
+      <NavBar />
+      <CardConntainer />
+    </main>
+  );
+}
+export default App;
